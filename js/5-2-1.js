cc7931e9e14ee0b660dfb9f26c8756c03fb308cc
hako <wetvulture@gmail.com>
2023-05-08 13:52:25 +0900

    my site complete

---
+++ js/5-2-1.js
@@ -0,0 +1,12 @@
+$(".openbtn").click(function () {
+    $(this).toggleClass('active');
+    $(".anime-box").toggleClass("display")
+});
+
+// console.log("hello world")
+
+// document.getElementById('openbtn').onclick = function (){
+//     console.log("hello world");
+//     document.getElementById('openbtn').classList.add("active");
+// }
+
